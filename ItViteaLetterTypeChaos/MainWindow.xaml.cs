6e4ba1f8b98357691f1b3984107e3c0a6a97570d
Stella van der burg <estellavanderburg@gmail.com>
2020-04-21 10:54:40 +0200

    Made base layout with Textbox and Buttons. With methods for loading in text as well as saving it in a txt file.

--- ItViteaLetterTypeChaos/MainWindow.xaml.cs
+++ ItViteaLetterTypeChaos/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,32 @@ namespace ItViteaLetterTypeChaos
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+        //public variables.
+        public string TextPlain, TextChaos;
+
+        private void Btn_Click_Import(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BoxTxt.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void Btn_Click_Export(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, BoxTxt.Text);
         }
     }
 }
